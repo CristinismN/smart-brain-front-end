585361292a1178f1dd04e08529aae743e7909ad7
CristinismN <chrystyna_n@yahoo.com>
2022-02-10 17:13:52 +0200

    testing without react-particles

--- src/App.js
+++ src/App.js
@@ -10,78 +10,78 @@ import Particles from "react-tsparticles";
 import './App.css';
 
 
-const particlesOptions = {
+// const particlesOptions = {
   
-        fpsLimit: 60,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: false,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 80,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 3,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true}
+//         fpsLimit: 60,
+//         interactivity: {
+//           events: {
+//             onClick: {
+//               enable: true,
+//               mode: "push",
+//             },
+//             onHover: {
+//               enable: true,
+//               mode: "repulse",
+//             },
+//             resize: false,
+//           },
+//           modes: {
+//             bubble: {
+//               distance: 400,
+//               duration: 2,
+//               opacity: 0.8,
+//               size: 40,
+//             },
+//             push: {
+//               quantity: 4,
+//             },
+//             repulse: {
+//               distance: 80,
+//               duration: 0.4,
+//             },
+//           },
+//         },
+//         particles: {
+//           color: {
+//             value: "#ffffff",
+//           },
+//           links: {
+//             color: "#ffffff",
+//             distance: 150,
+//             enable: true,
+//             opacity: 0.5,
+//             width: 1,
+//           },
+//           collisions: {
+//             enable: true,
+//           },
+//           move: {
+//             direction: "none",
+//             enable: true,
+//             outMode: "bounce",
+//             random: false,
+//             speed: 3,
+//             straight: false,
+//           },
+//           number: {
+//             density: {
+//               enable: true,
+//               area: 800,
+//             },
+//             value: 80,
+//           },
+//           opacity: {
+//             value: 0.5,
+//           },
+//           shape: {
+//             type: "circle",
+//           },
+//           size: {
+//             random: true,
+//             value: 5,
+//           },
+//         },
+//         detectRetina: true}
 
 const initialState = { 
       input: '',
